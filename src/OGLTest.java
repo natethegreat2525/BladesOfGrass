584c91ebca74fb48f039b560db80891130bfcb6b
Nathan Shirley <natethegreat2525@gmail.com>
2015-03-31 23:32:51 -0400

    added translation

--- src/OGLTest.java
+++ src/OGLTest.java
@@ -1,80 +1,88 @@
-
 import org.lwjgl.LWJGLException;
 import org.lwjgl.opengl.Display;
 import org.lwjgl.opengl.DisplayMode;
 import org.lwjgl.opengl.GL11;
 import org.lwjgl.input.Keyboard;
+import org.lwjgl.input.Mouse;
 
 public class OGLTest {
-    public void start() {
-        try {
-            Display.setDisplayMode(new DisplayMode(640, 480));
-            Display.create();
-        } catch (LWJGLException e) {
-            e.printStackTrace();
-            System.exit(0);
-        }
+	public void start() {
+		try {
+			Display.setDisplayMode(new DisplayMode(640, 480));
+			Display.create();
+		} catch (LWJGLException e) {
+			e.printStackTrace();
+			System.exit(0);
+		}
 
-        // Init OpenGL
-        GL11.glMatrixMode(GL11.GL_PROJECTION);
-        GL11.glLoadIdentity();
-        GL11.glOrtho(-3.2, 3.2, -2.4, 2.4, -1, 1);
-        GL11.glMatrixMode(GL11.GL_MODELVIEW);
+		// Init OpenGL
+		GL11.glMatrixMode(GL11.GL_PROJECTION);
+		GL11.glLoadIdentity();
+		GL11.glOrtho(-3.2, 3.2, -2.4, 2.4, -1, 1);
+		GL11.glMatrixMode(GL11.GL_MODELVIEW);
 
-        boolean quit = false;
+		boolean quit = false;
 
-        while (!quit) {         
-            // Clear the screen.
-            GL11.glClear(GL11.GL_COLOR_BUFFER_BIT | GL11.GL_DEPTH_BUFFER_BIT);
+		while (!quit) {
+			// Clear the screen.
+			GL11.glClear(GL11.GL_COLOR_BUFFER_BIT | GL11.GL_DEPTH_BUFFER_BIT);
 
-            // Begin drawing
-            GL11.glBegin(GL11.GL_TRIANGLES);
-            
-            
-            	//First triangle
-                // Top & Red
-                GL11.glColor3f(1.0f, 0.0f, 0.0f);
-                GL11.glVertex2f(0.0f, 1.0f);
+			GL11.glPushMatrix(); {
 
-                // Right & Green
-                GL11.glColor3f(0.0f, 1.0f, 0.0f);
-                GL11.glVertex2f(1.0f, 1.0f);
+				double value = Mouse.getX();
+				value = (value / 100) - 3.2;
+				GL11.glTranslated(value, 0, 0);
+				drawTriangles();
 
-                // Left & Blue
-                GL11.glColor3f(0.0f, 0.0f, 1.0f);
-                GL11.glVertex2f(1.0f, -1.0f);
-                
-                
-                
-                //second triangle
-             // Top & Red
-                GL11.glColor3f(1.0f, 0.0f, 0.0f);
-                GL11.glVertex2f(0.0f, -1.0f);
+			}
+			GL11.glPopMatrix();
 
-                // Right & Green
-                GL11.glColor3f(0.0f, 1.0f, 0.0f);
-                GL11.glVertex2f(-1.0f, -1.0f);
+			Display.update();
 
-                // Left & Blue
-                GL11.glColor3f(0.0f, 0.0f, 1.0f);
-                GL11.glVertex2f(-1.0f, 1.0f);
-                
-                
-                
-            GL11.glEnd();
+			if (Display.isCloseRequested()
+					|| Keyboard.isKeyDown(Keyboard.KEY_ESCAPE))
+				quit = true;
+		}
 
-            Display.update();
+		Display.destroy();
+	}
 
-            if (Display.isCloseRequested() || Keyboard.isKeyDown(Keyboard.KEY_ESCAPE))
-                quit = true;
-        }
+	public static void drawTriangles() {
+		// Begin drawing
+		GL11.glBegin(GL11.GL_TRIANGLES);
 
-        Display.destroy();
-    }
+		// First triangle
+		// Top & Red
+		GL11.glColor3f(1.0f, 0.0f, 0.0f);
+		GL11.glVertex2f(0.0f, 1.0f);
 
-    public static void main(String args[]) {
-        OGLTest ct = new OGLTest();
-        ct.start();
-    }
+		// Right & Green
+		GL11.glColor3f(0.0f, 1.0f, 0.0f);
+		GL11.glVertex2f(1.0f, 1.0f);
+
+		// Left & Blue
+		GL11.glColor3f(0.0f, 0.0f, 1.0f);
+		GL11.glVertex2f(1.0f, -1.0f);
+
+		// second triangle
+		// Top & Red
+		GL11.glColor3f(1.0f, 0.0f, 0.0f);
+		GL11.glVertex2f(0.0f, -1.0f);
+
+		// Right & Green
+		GL11.glColor3f(0.0f, 1.0f, 0.0f);
+		GL11.glVertex2f(-1.0f, -1.0f);
+
+		// Left & Blue
+		GL11.glColor3f(0.0f, 0.0f, 1.0f);
+		GL11.glVertex2f(-1.0f, 1.0f);
+
+		GL11.glEnd();
+	}
+
+	public static void main(String args[]) {
+		OGLTest ct = new OGLTest();
+		ct.start();
+	}
 
 }
