9b22ea1431488e596df21b30946fdf386d838094
Nathan Shirley <natethegreat2525@gmail.com>
2015-04-07 22:09:41 -0400

    changed blending and added alpha

--- src/OGLTest.java
+++ src/OGLTest.java
@@ -23,34 +23,32 @@ public class OGLTest {
 
 		//THIS LINE
 		GL11.glEnable(GL11.GL_BLEND);
-		GL11.glBlendFunc(GL11.GL_ONE, GL11.GL_ONE);
+		GL11.glBlendFunc(GL11.GL_SRC_ALPHA, GL11.GL_ONE_MINUS_SRC_ALPHA);
 		
 		boolean quit = false;
 		double angle = 0;
 
 		while (!quit) {
-			angle += 2;
+			angle += .5;
 			// Clear the screen.
 			GL11.glClear(GL11.GL_COLOR_BUFFER_BIT | GL11.GL_DEPTH_BUFFER_BIT);
 
 			GL11.glPushMatrix(); {
-				//calculates value
-				double value = Mouse.getX();
-				value = (value / 100) - 3.2;
-				GL11.glTranslated(value, 0, 0);
-				GL11.glRotated(angle, 0, 0, 1);
-				drawTriangles();
+				GL11.glRotated(0, 0, 0, 1);
+				drawTriangles(1);
 
 			}
 			GL11.glPopMatrix();
 			
 			GL11.glPushMatrix(); {
 				//calculates value
-				double value = Mouse.getY();
-				value = (value / 100) - 2.4;
-				GL11.glTranslated(0, value, 0);
+				double x = Mouse.getX();
+				double y = Mouse.getY();
+				x = (x / 100) - 3.2;
+				y = (y / 100) - 2.4;
+				GL11.glTranslated(x, y, 0);
 				GL11.glRotated(angle, 0, 0, 1);
-				drawTriangles();
+				drawTriangles(1);
 
 			}
 			GL11.glPopMatrix();
@@ -66,21 +64,21 @@ public class OGLTest {
 		Display.destroy();
 	}
 
-	public static void drawTriangles() {
+	public static void drawTriangles(float alpha) {
 		// Begin drawing
 		GL11.glBegin(GL11.GL_TRIANGLES);
 
 		// First triangle
 		// Top & Red
-		GL11.glColor3f(1.0f, 0.0f, 0.0f);
+		GL11.glColor4f(1.0f, 0.0f, 0.0f, alpha);
 		GL11.glVertex2f(0.0f, 1.0f);
 
 		// Right & Green
-		GL11.glColor3f(0.0f, 1.0f, 0.0f);
+		GL11.glColor4f(0.0f, 1.0f, 0.0f, alpha);
 		GL11.glVertex2f(1.0f, -0.8f);
 
 		// Left & Blue
-		GL11.glColor3f(0.0f, 0.0f, 1.0f);
+		GL11.glColor4f(0.0f, 0.0f, 1.0f, 0.5f);
 		GL11.glVertex2f(-1.0f, -0.8f);
 
 		GL11.glEnd();
